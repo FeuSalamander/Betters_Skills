9f21bbe04aaa05ca5ec2d07a086c9d09eed0aa04
FeuSalamander <ferreresarnaud@gmail.com>
2023-05-08 17:19:12 +0200

    updated skills textures

--- src/main/java/net/feusalamander/betterskills/client/gui/SkillsScreen.java
+++ src/main/java/net/feusalamander/betterskills/client/gui/SkillsScreen.java
@@ -6,10 +6,16 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.Minecraft;
 
 import net.feusalamander.betterskills.world.inventory.SkillsMenu;
+import net.feusalamander.betterskills.procedures.MiningLvlProcedure;
+import net.feusalamander.betterskills.procedures.ForagingLvlProcedure;
+import net.feusalamander.betterskills.procedures.FishingLvlProcedure;
+import net.feusalamander.betterskills.procedures.FarmingLvlProcedure;
+import net.feusalamander.betterskills.procedures.CombatLvlProcedure;
 import net.feusalamander.betterskills.network.SkillsButtonMessage;
 import net.feusalamander.betterskills.BetterskillsMod;
 
@@ -24,6 +30,11 @@ public class SkillsScreen extends AbstractContainerScreen<SkillsMenu> {
 	private final int x, y, z;
 	private final Player entity;
 	Button button_ability_config;
+	ImageButton imagebutton_mining;
+	ImageButton imagebutton_combat;
+	ImageButton imagebutton_foraging;
+	ImageButton imagebutton_farming;
+	ImageButton imagebutton_fishing;
 
 	public SkillsScreen(SkillsMenu container, Inventory inventory, Component text) {
 		super(container, inventory, text);
@@ -59,21 +70,6 @@ public class SkillsScreen extends AbstractContainerScreen<SkillsMenu> {
 		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/skillstextlow.png"));
 		this.blit(ms, this.leftPos + 4, this.topPos + 6, 0, 0, 100, 27, 100, 27);
 
-		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/fishing_rod.png"));
-		this.blit(ms, this.leftPos + 288, this.topPos + 82, 0, 0, 32, 32, 32, 32);
-
-		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/wooden_pick.png"));
-		this.blit(ms, this.leftPos + 29, this.topPos + 81, 0, 0, 32, 32, 32, 32);
-
-		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/wooden_sword.png"));
-		this.blit(ms, this.leftPos + 96, this.topPos + 82, 0, 0, 32, 32, 32, 32);
-
-		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/wood_axe.png"));
-		this.blit(ms, this.leftPos + 159, this.topPos + 83, 0, 0, 32, 32, 32, 32);
-
-		RenderSystem.setShaderTexture(0, new ResourceLocation("betterskills:textures/screens/wood_hoe.png"));
-		this.blit(ms, this.leftPos + 221, this.topPos + 83, 0, 0, 32, 32, 32, 32);
-
 		RenderSystem.disableBlend();
 	}
 
@@ -93,16 +89,21 @@ public class SkillsScreen extends AbstractContainerScreen<SkillsMenu> {
 
 	@Override
 	protected void renderLabels(PoseStack poseStack, int mouseX, int mouseY) {
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_mining_varmininglvl"), 27, 71, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_combat_varcombatlvl"), 89, 71, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_farming_varfarminglvl"), 219, 71, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_fishing_varfishinglvl"), 287, 70, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_foraging_varforaginglvl"), 157, 71, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_varforagingxp"), 157, 118, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_varcombatxp"), 89, 118, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_varminingxp"), 27, 118, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_varfarmingxp"), 219, 118, -12829636);
-		this.font.draw(poseStack, Component.translatable("gui.betterskills.skills.label_varfishingxp"), 287, 118, -12829636);
+		this.font.draw(poseStack,
+
+				MiningLvlProcedure.execute(entity), 43, 118, -12829636);
+		this.font.draw(poseStack,
+
+				CombatLvlProcedure.execute(entity), 103, 174, -12829636);
+		this.font.draw(poseStack,
+
+				ForagingLvlProcedure.execute(entity), 162, 164, -12829636);
+		this.font.draw(poseStack,
+
+				FarmingLvlProcedure.execute(entity), 220, 158, -12829636);
+		this.font.draw(poseStack,
+
+				FishingLvlProcedure.execute(entity), 282, 159, -12829636);
 	}
 
 	@Override
@@ -123,5 +124,25 @@ public class SkillsScreen extends AbstractContainerScreen<SkillsMenu> {
 		});
 		guistate.put("button:button_ability_config", button_ability_config);
 		this.addRenderableWidget(button_ability_config);
+		imagebutton_mining = new ImageButton(this.leftPos + 20, this.topPos + 82, 48, 48, 0, 0, 48, new ResourceLocation("betterskills:textures/screens/atlas/imagebutton_mining.png"), 48, 96, e -> {
+		});
+		guistate.put("button:imagebutton_mining", imagebutton_mining);
+		this.addRenderableWidget(imagebutton_mining);
+		imagebutton_combat = new ImageButton(this.leftPos + 85, this.topPos + 82, 48, 48, 0, 0, 48, new ResourceLocation("betterskills:textures/screens/atlas/imagebutton_combat.png"), 48, 96, e -> {
+		});
+		guistate.put("button:imagebutton_combat", imagebutton_combat);
+		this.addRenderableWidget(imagebutton_combat);
+		imagebutton_foraging = new ImageButton(this.leftPos + 150, this.topPos + 82, 48, 48, 0, 0, 48, new ResourceLocation("betterskills:textures/screens/atlas/imagebutton_foraging.png"), 48, 96, e -> {
+		});
+		guistate.put("button:imagebutton_foraging", imagebutton_foraging);
+		this.addRenderableWidget(imagebutton_foraging);
+		imagebutton_farming = new ImageButton(this.leftPos + 215, this.topPos + 82, 48, 48, 0, 0, 48, new ResourceLocation("betterskills:textures/screens/atlas/imagebutton_farming.png"), 48, 96, e -> {
+		});
+		guistate.put("button:imagebutton_farming", imagebutton_farming);
+		this.addRenderableWidget(imagebutton_farming);
+		imagebutton_fishing = new ImageButton(this.leftPos + 280, this.topPos + 82, 48, 48, 0, 0, 48, new ResourceLocation("betterskills:textures/screens/atlas/imagebutton_fishing.png"), 48, 96, e -> {
+		});
+		guistate.put("button:imagebutton_fishing", imagebutton_fishing);
+		this.addRenderableWidget(imagebutton_fishing);
 	}
 }
---
+++ src/main/java/net/feusalamander/betterskills/procedures/FishingLvlProcedure.java
@@ -0,0 +1,13 @@
+package net.feusalamander.betterskills.procedures;
+
+import net.minecraft.world.entity.Entity;
+
+import net.feusalamander.betterskills.network.BetterskillsModVariables;
+
+public class FishingLvlProcedure {
+	public static String execute(Entity entity) {
+		if (entity == null)
+			return "";
+		return new java.text.DecimalFormat("##").format((entity.getCapability(BetterskillsModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new BetterskillsModVariables.PlayerVariables())).FishingLvL);
+	}
+}
